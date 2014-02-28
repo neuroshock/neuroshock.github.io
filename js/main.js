72c8601d7ee0c56bae767b2301bae8daf43077f2
Mike Chung <neuroshock@gmail.com>
2014-02-27 23:54:34 -0600

    Fixed navbar visual selection

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-$('.nav li a').on('click', function(){
+$('.nav li a').on('click', function(eventObject){
   var navbar_toggle = $('.navbar-toggle');
   if (navbar_toggle.is(':visible')) {
     navbar_toggle.trigger('click');
   }
+
+  var oldActive = $('.nav li.active');
+  oldActive.removeClass('active');
+  $(eventObject.target).parent().addClass('active');
 });
 
